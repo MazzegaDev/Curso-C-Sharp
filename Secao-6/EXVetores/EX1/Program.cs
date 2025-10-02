b912d3b397212797cdc897de1e387b7498220d1d
gui <=>
2025-10-02 11:17:04 -0300

    Exercicio de vetor

---
+++ Secao-6/EXVetores/EX1/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+namespace EXVetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write($"Quantos quartos serao alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            Estudante[] quarto = new Estudante[10];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Aluguel #{i + 1}");
+                Console.Write($"Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write($"Email: ");
+                string email = Console.ReadLine();
+                Console.Write($"Quarto: ");
+                int nQuarto = int.Parse(Console.ReadLine());
+                quarto[nQuarto] = new Estudante(nome, email);
+            }
+
+            Console.WriteLine($"Quartos ocupados");
+            for (int i = 0; i < quarto.Length; i++)
+            {
+                if (quarto[i] != null)
+                {
+                    Console.WriteLine($"{i}: {quarto[i].Nome}, {quarto[i].Email}");
+                }
+            }
+        }
+    }
+}
