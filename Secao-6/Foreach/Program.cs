183a9b47f016efeb3894bb360492b5cef0ac5116
gui <=>
2025-10-03 09:54:49 -0300

    Laço foreach

---
+++ Secao-6/Foreach/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                ->Foreach
+                O laço foreach e usado para percorrer arrays sua leitura:
+                Para cada objeto em arr faça.
+
+                string[] arr = new string[] {"Maria", "Joao", "Alex"}
+                foreach(string obj in arr){}
+                
+                obj sendo o apelido para cada elemento no vetor
+            */
+            string[] arr = new string[] { "Maria", "Joao", "Alex" };
+            foreach (string obj in arr)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
