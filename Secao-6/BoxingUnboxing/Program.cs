f04dba0ad1d32c0214bd4403b3b0b401e8e34f98
gui <=>
2025-10-03 09:41:18 -0300

    Boxing e unboxing

---
+++ Secao-6/BoxingUnboxing/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace BoxingUnboxing
+{
+    class Program
+    {
+        /*
+            Boxing e unboxing
+            
+            ->Boxing 
+            Boxing e a conversao de um objeto tipo valor para um objeto tipo referencia compativel.
+            ->Unboxing e a conversao de um objeto tipo referencia para um objeto tipo valor compativel
+        */
+        static void Main(string[] args)
+        {
+            //Boxing
+            int x = 20;
+            Object obj = x;
+            // A variavel obj referencia para um objeto no heap
+
+            //Unboxing
+            int y = (int)obj; //So funciona se os tipos forem compativeis
+        }
+    }
+}
