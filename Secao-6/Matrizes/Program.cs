5fe0d4845ae12fd905266c6593bda9017081833e
gui <=>
2025-10-06 10:19:37 -0300

    Matriz parte 2 final

--- Secao-6/Matrizes/Program.cs
+++ Secao-6/Matrizes/Program.cs
@@ -15,15 +15,48 @@
                 Desvantagens:
                     >Tamanho fixo
                     >Dificuldade para se realizar inserçoes e deleçoes
+                Declarar uma matriz:
+                    >tipo, representação [,], variavel, instancia, quantidade de linhas e colunas.
+                    >double[,] mat = new double[2, 3];
+                Metodos:
+                    >Console.WriteLine(mat.Length);
+                    >Console.WriteLine(mat.Rank); //Retorna a quantidade de linhas
+                    >Console.WriteLine(mat.GetLength(0)); //Retorna o tamanho da primeira dimensão -> linhas
+                    >Console.WriteLine(mat.GetLength(1)); //Retorna o tamanho da segunda dimensão -> colunas
             */
-            //Declarar uma matriz:
-            // tipo, representação [,], variavel, instancia, quantidade de linhas e colunas.
-            double[,] mat = new double[2, 3];
-            Console.WriteLine(mat.Length);
-            Console.WriteLine(mat.Rank); //Retorna a quantidade de linhas
-            Console.WriteLine(mat.GetLength(0)); //Retorna o tamanho da primeira dimensão -> linhas
+            Console.WriteLine($"Informe o numero de linhas e colunas");
+            int n = int.Parse(Console.ReadLine());
 
-            Console.WriteLine(mat.GetLength(1)); //Retorna o tamanho da segunda dimensão -> colunas
+            int[,] mat = new int[n, n];
+            //Linhas
+            for (int l = 0; l < n; l++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                //Colunas
+                for (int c = 0; c < n; c++)
+                {
+                    //Preenche as linhas das colunas
+                    mat[l, c] = int.Parse(values[c]);
+                }
+            }
+            Console.WriteLine($"Main diagonal:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(mat[i, i] + " ");
+            }
+            Console.WriteLine();
+            int negatives = 0;
+            for (int l = 0; l < n; l++)
+            {
+                for (int c = 0; c < n; c++)
+                {
+                    if (mat[l, c] < 0)
+                    {
+                        negatives++;
+                    }
+                }
+            }
+            Console.WriteLine($"Negatives count: {negatives}");
         }
     }
 }
