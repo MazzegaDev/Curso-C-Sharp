a15a163ebbfb80e4eeff3bbd010fc36a3d0168dd
mazzegaDev <=>
2025-10-08 09:17:22 -0300

    Inferencia de tipo var

---
+++ Secao-7/InferenciaVAR/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace InferenciaVAR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Inferencia var
+                ->Var
+                Ela serve para inferir automaticamente o tipo da variavel conforme o valor atribuido a ela:
+                var x = 10 <- x é um int
+            */
+
+            var x = 10;
+            var y = 20.0;
+            var z = "Guilherme";
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+        }
+    }
+}
