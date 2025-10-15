917dd8c7159d2492c0db6a264793096c8dde46c5
MazzegaDev <=>
2025-10-15 14:43:10 -0300

    Composição pt2

---
+++ Secao-8/Composicao/Entities/Departament.cs
@@ -0,0 +1,17 @@
+
+namespace Composicao.Entities;
+
+public class Departament
+{
+    public string Name { get; set; }
+    
+    public Departament()
+    {
+        
+    }
+
+    public Departament(string nome)
+    {
+        Name = Name;
+    }
+}
---
+++ Secao-8/Composicao/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,8 @@
+namespace Composicao.Entities.Enums;
+
+public enum WorkerLevel : int
+{
+    Junior = 0,
+    MidLevel = 1,
+    Senior = 2,
+}
