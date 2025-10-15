5642d82498941fa2562dbd99dac2be2da03e43ac
MazzegaDev <=>
2025-10-15 14:11:06 -0300

    Composição parte um

---
+++ Secao-8/Composicao/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Composicao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Composição
+                ->É um tipo de associação que permite que um objeto contenha outro
+
+                Relação:
+                tem-um ou tem-varios
+
+                Vantagens
+                ->Organização = Divisão de responsabilidades
+                ->Coesao
+                ->Flexibilidade
+                ->Reuso
+            */
+        }
+    }
+}
